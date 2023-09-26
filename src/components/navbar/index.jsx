292e8cf27275b725e645563bc3e0ef8b065b15eb
xphbcx <xphbcx@gmail.com>
2023-09-26 13:20:02 -0300

    adição dropdown

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import DropDown from '../ItemListContainer'
 import Carrinho from '../CardWidget/carrinho'
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button } from '@mantine/core';
+
 
 export default function NavBar() {
 
+    function Demo() {
+        const [opened, { open, close }] = useDisclosure(false);
+      
+        return (
+          <>
+            <Drawer opened={opened} onClose={close} title="Authentication">
+              <h1>TESTE</h1>
+            </Drawer>
+      
+            <Button onClick={open}>Open Drawer</Button>
+          </>
+        );
+      }
     return (
         <div className="navbar">
             <div className="logo">AltStore</div>
@@ -16,6 +32,7 @@ export default function NavBar() {
             <div className="buttons">
                 <button>Login</button>
                 <Carrinho></Carrinho>
+                <Demo></Demo>
             </div>
         </div>
 
